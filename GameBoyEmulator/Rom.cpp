c8252406c23e526ad21728d480ee2deab9591c0a
Derek Drost <drostd4564@my.uwstout.edu>
2017-05-09 07:46:31 -0500

    Cart Load WIP

--- GameBoyEmulator/Rom.cpp
+++ GameBoyEmulator/Rom.cpp
@@ -11,11 +11,15 @@ Rom::Rom(std::string romPath) {
 	loadRom(romPath);
 }
 
+//TODO: Handle different sized carts
 void Rom::loadRom(std::string romPath) {
+	std::ifstream in(romPath, std::ifstream::ate | std::ifstream::binary);
+	std::ifstream::pos_type size = in.tellg();
+	in.close();
 	std::basic_fstream<unsigned char> romFile;
 	romFile.open(romPath, std::ios::in | std::ios::binary);
 	if (romFile.is_open()) {
-		romFile.read(romData, 0x8000);
+		romFile.read(romData, size);
 	}
 }
 
